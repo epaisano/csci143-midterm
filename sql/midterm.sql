69dd17b96eb2aec6899fe0aff942a3b493bed501
Emma Munoz <epaisano24@cmc.edu>
2024-03-17 12:43:34 -0700

    finished midterm

--- sql/midterm.sql
+++ sql/midterm.sql
@@ -14,9 +14,16 @@
  * so there is no need to do a breakdown of revenue per year.
  */
 
-SELECT 'enter your solution here';
 
-
+select sum(amount) as "total revenue", country from 
+country
+join city using (country_id)
+join address using (city_id)
+join customer using (address_id)
+join rental using (customer_id)
+join payment using (rental_id)
+where country = 'North Korea'
+group by country;
 
 
 
@@ -26,16 +33,42 @@ SELECT 'enter your solution here';
  * and so they want to know which family-friendly actors generate the most revenue.
  *
  * Write a SQL query that:
- * Lists the first and last names of all actors who have appeared in movies in the "Family" category,
+ * Lists the first and last names of all s who have appeared in movies in the "Family" category,
  * but that have never appeared in movies in the "Horror" category.
  * For each actor, you should also list the total amount that customers have paid to rent films that the actor has been in.
  * Order the results so that actors generating the most revenue are at the top.
  */
 
-SELECT 'enter your solution here';
 
+select first_name, last_name, total_paid from 
+    (select actor_id, sum(amount) as total_paid from
+        (select first_name, last_name, actor_id
+         from actor 
+         join film_actor using (actor_id)
+         join film using (film_id)
+         join film_category using (film_id)
+         join category using (category_id)
+         where name = 'Family'
 
+         EXCEPT
 
+         select first_name, last_name, actor_id
+         from actor 
+         join film_actor using (actor_id)
+         join film using (film_id)
+         join film_category using (film_id)
+         join category using (category_id)
+         where name = 'Horror'
+         ) as t
+    join film_actor using (actor_id)
+    join film using (film_id)
+    join inventory using (film_id)
+    join rental using (inventory_id)
+    join payment using (rental_id)
+    group_by actor_id
+) as q
+join actor using (actor_id)
+order by total_paid desc;
 
 
 
@@ -47,10 +80,30 @@ SELECT 'enter your solution here';
  * but have never co-starred with RUSSEL BACALL in any movie.
  */
 
-SELECT 'enter your solution here';
 
+select first_name || ' ' || last_name as "Actor Name" from
+        (select actor_id, first_name, last_name
+        from actor
+        join film_actor using (actor_id)
+        join film using (film_id)
+        where title = 'AGENT TRUMAN'
+        ) as y
+        
+EXCEPT
 
-
+select "Actor Name" from
+        (select actor_id, first_name || ' ' || last_name as "Actor Name" from
+                (select t.actor_id, actor.first_name as first_name, actor.last_name as last_name from                   (SELECT DISTINCT mc.actor_id
+                   FROM film_actor mc
+                   JOIN film m ON mc.film_id = m.film_id
+                   JOIN film_actor mc_bacon ON m.film_id = mc_bacon.film_id
+                   JOIN actor a ON mc_bacon.actor_id = a.actor_id
+                   WHERE mc_bacon.actor_id = 112
+                   ) as t
+                join actor on t.actor_id=actor.actor_id
+                ) as w
+        ) as z
+where "Actor Name" != 'RUSSELL BACALL';
 
 
 
@@ -68,9 +121,24 @@ SELECT 'enter your solution here';
  * Your results should not contain any duplicate titles.
  */
 
-SELECT 'enter your solution here';
 
+select title
+from film 
+where title not ilike '%f%'
 
+intersect
 
+select distinct film.title
+from actor
+join film_actor using (actor_id)
+join film using (film_id)
+where actor.first_name not ilike '%f%' and actor.last_name not ilike '%f%'
 
+intersect
 
+select distinct film.title
+from customer 
+join rental using (customer_id)
+join inventory using (inventory_id)
+join film using (film_id)
+where customer.first_name not ilike '%f%' and customer.last_name not ilike '%f%';
